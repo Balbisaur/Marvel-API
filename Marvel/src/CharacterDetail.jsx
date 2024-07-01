8f77e84f9000f828f1e57d5c1cc9e3a46c73c33f
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-06-30 19:24:55 -0500

    Update CharacterDetail.jsx

--- Marvel/src/CharacterDetail.jsx
+++ Marvel/src/CharacterDetail.jsx
@@ -15,8 +15,8 @@ const CharacterDetail = ({ characterId }) => {
                         {
                           params: {
                             ts: '1',
-                            apikey: '5fd9ca13146465fb78bdc29c7f391567',
-                            hash: '1e6d9386272336e8e1440bb2a96f98f0',
+                            apikey: '30d25ddacc2cbe54e2a70aa61f9d4e07',
+                            hash: '31f7f86db64459ce4cdd5a397c196119',
                           },
                     });
                     setCharacter(response.data.data.results[0]);
